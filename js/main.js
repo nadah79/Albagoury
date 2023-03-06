588ef8d6178909ad3dc048c1894c28860afc7773
nadah79 <nadah.hesham.sh@gmail.com>
2023-03-06 21:17:25 +0200

    add lawyer,news and about pages

--- js/main.js
+++ js/main.js
@@ -27,3 +27,25 @@ function showSlides() {
   slides[slideIndex-1].style.display = "flex";  
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+// news slider
+
+let newslideIndex = 0;
+showNewsSlides();
+
+function showNewsSlides() {
+  let i;
+  let newsSlides = document.getElementsByClassName("news-slides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < newsSlides.length; i++) {
+    newsSlides[i].style.display = "none";  
+  }
+  newslideIndex++;
+  if (newslideIndex > newsSlides.length) {newslideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  newsSlides[newslideIndex-1].style.display = "block";  
+  dots[newslideIndex-1].className += " active";
+  setTimeout(showNewsSlides, 2000); // Change image every 2 seconds
+}
+
