9e231208660fa8fcb9a82ee4d45cbc987a8b6b20
nadah79 <nadah.hesham.sh@gmail.com>
2023-03-11 00:44:56 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // nav
-let hamburger=document.querySelector(".hamburger");
-let navMenu =document.querySelector(".nav-menu");
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click",()=>{
-    navMenu.classList.toggle("show");
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
 });
 
-document.querySelectorAll(".nav-link").forEach(n=>
-    n.addEventListener("click",()=>{
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("show");
-    }))
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("show");
+  })
+);
 
-
-    // slider
+// slider
 let slideIndex = 0;
 showSlides();
 
@@ -21,14 +21,15 @@ function showSlides() {
   let i;
   let slides = document.getElementsByClassName("slides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "flex";  
-  setTimeout(showSlides, 2000); // Change  every 2 seconds
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  setTimeout(showSlides, 5000); // Change  every 2 seconds
 }
-
 
 // news slider
 let newslideIndex = 1;
@@ -38,29 +39,32 @@ function showNewsSlides(n) {
   let i;
   let newsSlides = document.getElementsByClassName("news-slides");
   let dots = document.getElementsByClassName("dot");
-  if (n > newsSlides.length) {newslideIndex = 1}  ;
-  if (n < 1) {newslideIndex = newsSlides.length};
+  if (n > newsSlides.length) {
+    newslideIndex = 1;
+  }
+  if (n < 1) {
+    newslideIndex = newsSlides.length;
+  }
   for (i = 0; i < newsSlides.length; i++) {
-    newsSlides[i].style.display = "none";  
+    newsSlides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  newsSlides[newslideIndex-1].style.display = "block";  
-  dots[newslideIndex-1].className += " active";
+  newsSlides[newslideIndex - 1].style.display = "block";
+  dots[newslideIndex - 1].className += " active";
   // setTimeout(showNewsSlides, 2000); // Change every 2 seconds
 }
 function currentSlide(n) {
-  showNewsSlides(newslideIndex=n);
+  showNewsSlides((newslideIndex = n));
 }
-
-
 
 // top btn
 let topBtn = document.getElementById("topBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
@@ -70,8 +74,35 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal-1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
